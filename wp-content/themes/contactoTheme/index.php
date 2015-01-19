9948d354c04c5097cd0e7dd6feab6c394439e9c8
Freddy H <freddy01288@gmail.com>
2015-01-19 18:47:12 -0500

    instalando theme

---
+++ wp-content/themes/contactoTheme/index.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+  <div class="row">
+    <div class="small-12 columns frase">
+      <span>"No queremos ser enciclopedias que se conocen hasta el segundo apellido de cada jugador de liga turca. Queremos jugar fútbol. Hablar de él. Compartir la alegría de un golazo"</span>
+      <br>
+      <a href="">Ver manifiesto</a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="small-12 medium-8 columns">
+      <iframe height="400" src="//www.youtube.com/embed/HJqEaxwlvLM?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="small-12 medium-4 columns formulario">
+      <div class="row">
+        <h3>Ficha de suscripción</h3>
+        <div class="small-6 columns">
+          <p>Nombres</p>
+          <input type="text"></input>
+          <p>Correo Electrónico</p>
+          <input type="text"></input>
+          <p>Distrito</p>
+          <input type="text"></input>
+        </div>
+        <div class="small-6 columns">
+          <p>Apellidos</p>
+          <input type="text"></input>
+          <p>Fecha de nacimiento</p>
+          <input type="text"></input>
+          <p>Dirección</p>
+          <input type="text"></input>
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-12 columns">
+          <p>Referencias</p>
+          <textarea></textarea>
+          <a href="#" class="button expand">Me suscribo</a>
+          <p class="info">Si vives en provincia o fuera de Lima, la revista te llegará de formato digital a tu correo electrónico.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php get_footer(); ?>
