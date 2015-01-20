4670af3148fb58273edd1b690d4d913c32ae399b
Freddy H <freddy01288@gmail.com>
2015-01-20 10:18:08 -0500

    agregando pagina manifiesto

---
+++ wp-content/themes/contactoTheme/page-manifiesto.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+<div class="row">
+	<div class="small-12 medium-10 medium-centered large-8 columns" role="main">
+
+		<div class="text-center logo">
+			<img src="<?php bloginfo('template_directory'); ?>/img/futbolfemenino_logo.png">
+		</div>
+
+		<?php //echo do_shortcode("[mc4wp_form]"); ?>
+		
+    <div class="content">
+	    <h1>manifiesto</h1>
+	    <p><strong>FUTBOLFEMENINO.PE es una revista que promueve la</strong></p> 
+	    <p>participaci&#243;n femenina en el deporte m&#225;s hermoso del mundo. Es decir, con nuestra presencia, lo hacemos</p>
+	    <p><em>m&#225;s hermoso todav&#237;a.</em></p>
+	    <p><strong>FUTBOLFEMENINO.PE es un movimiento joven que ha crecido amando el </strong></p>
+	    <p>f&#250;tbol. A todos los incautos, ent&#233;rense,</p>
+	    <p><em>no nos aburren los partidos, s&#243;lo los equipos malos.</em></p>
+	    <p>As&#237; como somos capaces de pasarnos horas viendo tiendas en un centro comercial, tambi&#233;n podemos mirar el desenlace del decentralizado y la champions league.</p>
+	    <p><strong>En FUTBOLFEMENINO.PE admiramos a todas las futbolistas amateur y </strong></p>
+	    <p>profesionales que nos abrieron la cancha a las que venimos atr&#225;s.</p>
+	    <p>De ellas hemos aprendido que</p>
+	    <p><em>un moret&#243;n en la pierna,</em></p>
+	    <p><em>no es una raz&#243;n para dejar de ponerse falda.</em></p>
+	    <p>Porque si est&#225;s dispuesta hasta a bloquear un pelotazo con la cara,</p> 
+	    <p><em>no puedes temerle a nada.</em></p>
+	    <p><strong>En FUTBOLFEMENINO.PE no queremos mostrarnos como las enciclopedias </strong></p>
+	    <p>que conocen hasta el segundo apellido de cada jugador de la liga turca. Buscamos ser lo opuesto a los medios manejados por hombres y para hombres. Vamos a hacer una revista&nbsp;sobre</p>
+	    <p><em>f&#250;tbol para mujeres que hasta los hombres van a querer ver.</em></p>
+	    <p><strong><em>Queremos jugar f&#250;tbol. Hablar de &#233;l. Compartir la pasi&#243;n de anotar&nbsp;un golazo.</em></strong></p>
+	    <p>Celebrar cuando tu equipo gana&#8212; y bien&#8212; al rival de turno. No ganaremos lo mismo que Cristiano Ronaldo, pero tenemos otras ventajas. En la cancha somos aguerridas y demostramos garra, pero <em>cuando dejamos los chimpunes y nos vestimos de gala ninguna pasa despercibida.&nbsp;</em></p>
+	    <p><br /></p>
+	    <p>Somos FUTBOLFEMENINO.PE&nbsp;</p>
+	    <p>y nosotras la movemos mejor.</p>
+    </div>
+
+		<?php //echo do_shortcode("[mc4wp_form]"); ?>
+
+		<div class="row">
+			<div class="small-12 small-centered medium-4 columns">
+				<?php //echo do_shortcode("[juiz_sps buttons='facebook, twitter, pinterest']"); ?>
+			</div>
+		</div>
+
+	</div>
+</div>
+<?php get_footer(); ?>
