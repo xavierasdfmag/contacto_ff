eaf2d8eafa0b1cdd54762b45cd1ab2d44f6e3e79
Freddy H <freddy01288@gmail.com>
2015-01-20 14:11:59 -0500

    modificando manifiesto

--- wp-content/themes/contactoTheme/page-manifiesto.php
+++ wp-content/themes/contactoTheme/page-manifiesto.php
@@ -34,13 +34,19 @@
 	    <p>y nosotras la movemos mejor.</p>
     </div>
 
-		<?php echo do_shortcode("[mc4wp_form]"); ?>
+		
+				<?php //echo do_shortcode("[juiz_sps buttons='facebook, twitter, pinterest']"); ?>
+		<div class="row">
+			<div class="small-12 small-centered medium-4 columns">
+				<?php echo do_shortcode("[mc4wp_form]"); ?>
+			</div>
+		</div>
 
 		<div class="row">
 			<div class="small-12 small-centered medium-4 columns">
-				<?php //echo do_shortcode("[juiz_sps buttons='facebook, twitter, pinterest']"); ?>
+				<?php echo do_shortcode('[contact-form-7 id="8" title="subscription"]'); ?>
 			</div>
-		</div>
+		</div>		
 
 	</div>
 </div>
