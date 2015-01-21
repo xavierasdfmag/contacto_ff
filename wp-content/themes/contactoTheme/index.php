4ade33fd0c9c895de9fc9d5ecc8aadcbdf7cb895
Freddy H <freddy01288@gmail.com>
2015-01-21 10:33:00 -0500

    probando integracion facebook

--- wp-content/themes/contactoTheme/index.php
+++ wp-content/themes/contactoTheme/index.php
@@ -1,4 +1,23 @@
 <?php get_header(); ?>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#share_button').live('click', function(e){
+      e.preventDefault();
+      FB.ui(
+      {
+      method: 'feed',
+      name: 'HyperArts Blog',
+      link: 'http://hyperarts.com/blog',
+      picture: 'http://www.hyperarts.com/_img/TabPress-LOGO-Home.png',
+      caption: 'I love HyperArts tutorials',
+      description: 'The HyperArts Blog provides tutorials for all things Facebook',
+      message: ''
+      });
+    });
+  });
+</script>
+
   <div class="row">
     <div class="small-12 columns frase">
       <span>"No queremos ser enciclopedias que se conocen hasta el segundo apellido de cada jugador de liga turca. Queremos jugar fútbol. Hablar de él. Compartir la alegría de un golazo"</span>
@@ -9,6 +28,7 @@
   <div class="row">
     <div class="small-12 medium-8 columns">
       <iframe height="400" src="//www.youtube.com/embed/HJqEaxwlvLM?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+      <img src = "share_button.png" id = "share_button">
     </div>
     <div class="small-12 medium-4 columns formulario">
       <?php echo do_shortcode('[contact-form-7 id="8" title="subscription"]'); ?>
