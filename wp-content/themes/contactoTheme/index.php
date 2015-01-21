bd0ae737c654340297d42e64c230eb673375a042
Freddy H <freddy01288@gmail.com>
2015-01-21 18:58:21 -0500

    mejorando modal

--- wp-content/themes/contactoTheme/index.php
+++ wp-content/themes/contactoTheme/index.php
@@ -18,7 +18,7 @@
     <div class="small-12 columns frase">
       <span>"No queremos ser enciclopedias que se conocen hasta el segundo apellido de cada jugador de liga turca. Queremos jugar fútbol. Hablar de él. Compartir la alegría de un golazo"</span>
       <br>
-      <a href="futbolfemenino.pe/contacto/?page_id=5">Ver manifiesto</a>
+      <a href="http://futbolfemenino.pe/contacto/?page_id=5">Ver manifiesto</a>
     </div>
   </div>
   <div class="row">
