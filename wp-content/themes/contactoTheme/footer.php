17e74145ab83666c9c5661e690aa5795e7e4b688
Freddy H <freddy01288@gmail.com>
2015-01-21 10:53:44 -0500

    probando integracion facebook

--- wp-content/themes/contactoTheme/footer.php
+++ wp-content/themes/contactoTheme/footer.php
@@ -3,7 +3,7 @@
 	<?php do_action('foundationPress_before_footer'); ?>
 	<?php dynamic_sidebar("footer-widgets"); ?>
 	<?php do_action('foundationPress_after_footer'); ?>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
+
 </footer>
 <a class="exit-off-canvas"></a>
 
@@ -11,6 +11,7 @@
 	</div>
 </div>
 <?php wp_footer(); ?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
 <?php do_action('foundationPress_before_closing_body'); ?>
 </body>
 </html>
