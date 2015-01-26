2e029397c4702b4241cfd8167d5e6ba7ac2550bc
Freddy H <freddy01288@gmail.com>
2015-01-26 12:44:07 -0500

    agregando script seguimiento facebook

--- wp-content/themes/contactoTheme/header.php
+++ wp-content/themes/contactoTheme/header.php
@@ -31,6 +31,24 @@
 		  ga('send', 'pageview');
 
 		</script>		
+
+		<!-- Facebook Conversion Code for Suscribete -->
+		<script>(function() {
+		  var _fbq = window._fbq || (window._fbq = []);
+		  if (!_fbq.loaded) {
+		    var fbds = document.createElement('script');
+		    fbds.async = true;
+		    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+		    var s = document.getElementsByTagName('script')[0];
+		    s.parentNode.insertBefore(fbds, s);
+		    _fbq.loaded = true;
+		  }
+		})();
+		window._fbq = window._fbq || [];
+		window._fbq.push(['track', '6020573656236', {'value':'0.01','currency':'PEN'}]);
+		</script>
+		<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6020573656236&amp;cd[value]=0.01&amp;cd[currency]=PEN&amp;noscript=1" /></noscript>		
+		<!-- Fin Facebook Conversion Code for Suscribete -->
 		
 		<meta property="og:url" content="http://futbolfemenino.pe/unete/" /> 
 		<meta property="og:title" content="ÚNETE A LA HINCHADA" />
